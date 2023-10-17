2e43a17e29b938db49f732fe8b2bd9465046a88c
victor-aa <victor.andrieux.arena@gmail.com>
2023-10-17 14:33:44 +0200

    Installation Nuxt JS

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Header from './header'
+import Footer from './footer'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+        <Header></Header>
+        <Footer></Footer>
+    </main>
+  )
+}
