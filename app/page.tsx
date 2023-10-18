5cc0ebdb9938fb5885dcd100aef145788f6671cc
sellay1 <nopleyzz@gmail.com>
2023-10-18 16:57:45 +0200

    itération 1 : Mise en place de sculpt sur une landingpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Header from './header'
+import Sculpt from './sculpt'
 import Footer from './footer'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
         <Header></Header>
+        <Sculpt></Sculpt>
         <Footer></Footer>
     </main>
   )
