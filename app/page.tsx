d0311fbd6e8160cef09b1d77b138ec9bea758ea7
victor-aa <victor.andrieux.arena@gmail.com>
2023-10-18 14:07:42 +0200

    Three JS dans REACTJS

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import Image from 'next/image'
-import Header from './header'
-import Footer from './footer'
+import Header from './header';
+import Footer from './footer';
+import Sculpt from './sculpt';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
         <Header></Header>
+        <Sculpt></Sculpt>
         <Footer></Footer>
     </main>
   )
 }
+  
---
+++ app/sculpt.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useRef, useEffect } from 'react';
+import * as THREE from 'three';
+const Sculpt: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      camera.position.z = 5;
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      containerRef.current?.appendChild(renderer.domElement);
+      const geometry = new THREE.BoxGeometry();
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+      const animate = () => {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+      };
+      animate();
+    }
+  }, []);
+  return <div ref={containerRef} />;
+};
+export default Sculpt;
