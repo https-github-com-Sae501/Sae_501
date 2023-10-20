616df7e26f3f82fd49659c6fc0996283608f7091
victor-aa <victor.andrieux.arena@gmail.com>
2023-10-20 09:25:59 +0200

    Correction affichage canvas

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="flex flex-row gap-2 p-2 px-3 bg-black text-white w-full text-xs">
-      <p className=''>
-        Copyright 2023
-      </p>
-      <p className=''>
-        -
-      </p>
-      <p className=''>
-        All rights reserved
-      </p>
-      <p className=''>
-        -
-      </p>
-      <p className=''>
-        Legal notice
-      </p>
+    <footer className="fixed bottom-0 w-full">
+        <div className="flex flex-row gap-2 p-2 px-3 bg-black text-white w-full text-xs">
+            <p className=''>
+                Copyright 2023
+            </p>
+            <p className=''>
+                -
+            </p>
+            <p className=''>
+                All rights reserved
+            </p>
+            <p className=''>
+                -
+            </p>
+            <p className=''>
+                Legal notice
+            </p>
+        </div>
+      
     </footer>
   );
 };
