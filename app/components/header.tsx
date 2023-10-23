7da0b08c41be4493d4733f2b355a6f5fbc3e43e8
victor-aa <victor.andrieux.arena@gmail.com>
2023-10-23 11:47:50 +0200

    Routage avec App router

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
                     </Link> 
                 </div>
                 <div className="text-sm">
-                    <Link href="/library">
+                    <Link href="./library">
                         Library
                     </Link> 
                 </div>
--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from './components/header'
-import Footer from './components/footer'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
 const LibraryPage = () => {
   return (
     <div className="bg-white h-screen w-screen">
     <Header></Header>
-      <h1 className="text-4xl font-bold p-4">Library</h1>
+      <h1 className="text-4xl font-bold p-4 pt-[3rem]">Library</h1>
     <Footer></Footer>
     </div>
   );
